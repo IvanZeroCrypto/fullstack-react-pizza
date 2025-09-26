ed6cdfc41c048942017440b5ef6860e240044101
IvanZeroCrypto <kirdini@inbox.ru>
2025-09-26 09:53:10 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,10 +13,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: [
-      "http://localhost:5173",
-      "https://react-pizza-blond-nine.vercel.app",
-    ],
+    origin: ["http://localhost:5173", "https://react-pizza-coral.vercel.app"],
   })
 );
 
